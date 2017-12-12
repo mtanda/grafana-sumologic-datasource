723805907fa0f908210247342088cdb3d923730d
Mitsuhiro Tanda <mitsuhiro.tanda@gmail.com>
2017-12-12 21:02:08 +0900

    fix annotation query editor

---
+++ src/annotations_query_ctrl.js
@@ -0,0 +1,8 @@
+export class SumologicAnnotationsQueryCtrl {
+
+  constructor($scope, $injector) {
+    this.scope = $scope;
+  }
+}
+
+SumologicAnnotationsQueryCtrl.templateUrl = 'partials/annotations.editor.html';
--- src/module.js
+++ src/module.js
@@ -1,5 +1,6 @@
 import {SumologicDatasource} from './datasource';
 import {SumologicQueryCtrl} from './query_ctrl';
+import {SumologicAnnotationsQueryCtrl} from './annotations_query_ctrl';
 
 class SumologicConfigCtrl {}
 SumologicConfigCtrl.templateUrl = 'partials/config.html';
@@ -7,5 +8,6 @@ SumologicConfigCtrl.templateUrl = 'partials/config.html';
 export {
   SumologicDatasource as Datasource,
   SumologicConfigCtrl as ConfigCtrl,
-  SumologicQueryCtrl as QueryCtrl
+  SumologicQueryCtrl as QueryCtrl,
+  SumologicAnnotationsQueryCtrl as AnnotationsQueryCtrl
 };
