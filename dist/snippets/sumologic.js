a97215408f5cc0122f3f9fd5a67e46e2e27019c8
Mitsuhiro Tanda <mitsuhiro.tanda@gmail.com>
2018-07-14 22:20:50 +0900

    build

--- dist/snippets/sumologic.js
+++ dist/snippets/sumologic.js
@@ -1,26 +1,15 @@
+// jshint ignore: start
+// jscs: disable
+ace.define("ace/snippets/sumologic",["require","exports","module"], function(require, exports, module) {
 "use strict";
 
-System.register([], function (_export, _context) {
-  "use strict";
+// exports.snippetText = "# rate\n\
+// snippet r\n\
+//   rate(${1:metric}[${2:range}])\n\
+// ";
 
-  return {
-    setters: [],
-    execute: function () {
-      // jshint ignore: start
-      // jscs: disable
-      ace.define("ace/snippets/sumologic", ["require", "exports", "module"], function (require, exports, module) {
-        "use strict";
+exports.snippets = [
+];
 
-        // exports.snippetText = "# rate\n\
-        // snippet r\n\
-        //   rate(${1:metric}[${2:range}])\n\
-        // ";
-
-        exports.snippets = [];
-
-        exports.scope = "sumologic";
-      });
-    }
-  };
+exports.scope = "sumologic";
 });
-//# sourceMappingURL=sumologic.js.map
