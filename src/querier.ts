fa57603d4e7edc7e3e70879330775ca8e3c6f03e
Mitsuhiro Tanda <mitsuhiro.tanda@gmail.com>
2019-03-18 15:12:08 +0900

    use async/await

--- src/querier.ts
+++ src/querier.ts
@@ -35,19 +35,18 @@ export class SumologicQuerier {
         this.maximumOffset = 10000;
     }
 
-    getResult() {
+    async getResult() {
         this.startTime = new Date();
-        if (!this.useObservable) {
-            return this.delay(() => {
-                return this.transition('CREATE_SEARCH_JOB');
-            }, Math.random() * 1000);
-        } else {
-            return Observable.defer(() => {
-                return this.delay(() => {
-                    return this.transition('CREATE_SEARCH_JOB');
-                }, Math.random() * 1000);
-            });
-        }
+        await this.delay(Math.random() * 1000);
+        return this.transition('CREATE_SEARCH_JOB');
+    }
+
+    getResultObservable() {
+        this.startTime = new Date();
+        return Observable.defer(async () => {
+            await this.delay(Math.random() * 1000);
+            return this.transition('CREATE_SEARCH_JOB');
+        });
     }
 
     transition(state) {
@@ -60,38 +59,34 @@ export class SumologicQuerier {
         }
     }
 
-    retry() {
+    async retry() {
         this.retryCount += 1;
-        return this.delay(() => {
-            if (!this.useObservable) {
-                return this.loop();
-            } else {
-                return this.loopForObservable();
-            }
-        }, this.calculateRetryWait(1000, this.retryCount));
+        await this.delay(this.calculateRetryWait(1000, this.retryCount));
+        if (!this.useObservable) {
+            return await this.loop();
+        } else {
+            return await this.loopForObservable();
+        }
     }
 
-    loop() {
+    async loop() {
         if (this.job) {
             let now = new Date();
             if (now.valueOf() - this.startTime.valueOf() > (this.timeoutSec * 1000)) {
                 console.error('timeout');
-                return this.doRequest('DELETE', '/v1/search/jobs/' + this.job.data.id).then((result) => {
-                    return Promise.reject({ message: 'timeout' });
-                });
+                await this.doRequest('DELETE', '/v1/search/jobs/' + this.job.data.id);
+                return Promise.reject({ message: 'timeout' });
             }
         }
 
         switch (this.state) {
             case 'CREATE_SEARCH_JOB':
-                return this.doRequest('POST', '/v1/search/jobs', this.params).then((job) => {
-                    this.job = job;
-                    return this.transition('REQUEST_STATUS');
-                });
+                this.job = await this.doRequest('POST', '/v1/search/jobs', this.params);
+                return this.transition('REQUEST_STATUS');
                 break;
             case 'REQUEST_STATUS':
-                return this.doRequest('GET', '/v1/search/jobs/' + this.job.data.id).then((status) => {
-                    this.status = status;
+                try {
+                    this.status = await this.doRequest('GET', '/v1/search/jobs/' + this.job.data.id);
                     if (this.status.data.state !== 'DONE GATHERING RESULTS') {
                         if (this.retryCount < 20) {
                             return this.retry();
@@ -111,7 +106,7 @@ export class SumologicQuerier {
                         return Promise.reject({ message: message });
                     }
                     return this.transition('REQUEST_RESULTS');
-                }).catch((err) => {
+                } catch (err) {
                     if (err.data && err.data.code && err.data.code === 'unauthorized') {
                         return Promise.reject(err);
                     }
@@ -121,7 +116,7 @@ export class SumologicQuerier {
                     } else {
                         return Promise.reject(err);
                     }
-                });
+                }
                 break;
             case 'REQUEST_RESULTS':
                 if (this.format === 'time_series_records' || this.format === 'records') {
@@ -129,52 +124,48 @@ export class SumologicQuerier {
                         return Promise.resolve([]);
                     }
                     let limit = Math.min(this.maximumOffset, this.status.data.recordCount);
-                    return this.doRequest('GET', '/v1/search/jobs/' + this.job.data.id + '/records?offset=0&limit=' + limit).then((response) => {
-                        return response.data;
-                    });
+                    let response = await this.doRequest('GET', '/v1/search/jobs/' + this.job.data.id + '/records?offset=0&limit=' + limit);
+                    return response.data;
                 } else if (this.format === 'messages') {
                     if (this.status.data.messageCount === 0) {
                         return Promise.resolve([]);
                     }
                     let limit = Math.min(this.maximumOffset, this.status.data.messageCount);
-                    return this.doRequest('GET', '/v1/search/jobs/' + this.job.data.id + '/messages?offset=0&limit=' + limit).then((response) => {
-                        return response.data;
-                    });
+                    let response = await this.doRequest('GET', '/v1/search/jobs/' + this.job.data.id + '/messages?offset=0&limit=' + limit);
+                    return response.data;
                 } else {
                     return Promise.reject({ message: 'unsupported type' });
                 }
                 break;
         }
+        return Promise.reject({ message: 'unexpected status' });
     }
 
-    loopForObservable() {
+    async loopForObservable() {
         if (this.job) {
             let now = new Date();
             if (now.valueOf() - this.startTime.valueOf() > (this.timeoutSec * 1000)) {
                 console.error('timeout');
-                return this.doRequest('DELETE', '/v1/search/jobs/' + this.job.data.id).then((result) => {
-                    return Promise.reject({ message: 'timeout' });
-                });
+                await this.doRequest('DELETE', '/v1/search/jobs/' + this.job.data.id);
+                return Observable.throw({ message: 'timeout' });
             }
         }
 
         switch (this.state) {
             case 'CREATE_SEARCH_JOB':
-                return this.doRequest('POST', '/v1/search/jobs', this.params).then((job) => {
-                    this.job = job;
-                    return this.transition('REQUEST_STATUS');
-                });
+                this.job = await this.doRequest('POST', '/v1/search/jobs', this.params);
+                return this.transition('REQUEST_STATUS');
                 break;
             case 'REQUEST_STATUS':
-                return this.doRequest('GET', '/v1/search/jobs/' + this.job.data.id).then((status) => {
-                    this.status = status;
+                try {
+                    this.status = await this.doRequest('GET', '/v1/search/jobs/' + this.job.data.id);
                     let prevMessageCount = this.messageCount;
                     let prevRecordCount = this.recordCount;
                     this.messageCount = this.status.data.messageCount;
                     this.recordCount = this.status.data.recordCount;
 
                     if (!_.isEmpty(this.status.data.pendingErrors) || !_.isEmpty(this.status.data.pendingWarnings)) {
-                        return Promise.reject({ message: this.status.data.pendingErrors.concat(this.status.data.pendingWarnings).join('\n') });
+                        return Observable.throw({ message: this.status.data.pendingErrors.concat(this.status.data.pendingWarnings).join('\n') });
                     }
 
                     if (this.status.data.state === 'DONE GATHERING RESULTS') {
@@ -189,20 +180,19 @@ export class SumologicQuerier {
                     }
 
                     // wait for new result arrival
-                    return this.delay(() => {
-                        return this.transition('REQUEST_STATUS');
-                    }, 200);
-                }).catch((err) => {
+                    await this.delay(200);
+                    return this.transition('REQUEST_STATUS');
+                } catch (err) {
                     if (err.data && err.data.code && err.data.code === 'unauthorized') {
-                        return Promise.reject(err);
+                        return Observable.throw(err);
                     }
                     // need to wait until job is created and registered
                     if (this.retryCount < 6 && err.data && err.data.code && err.data.code === 'searchjob.jobid.invalid') {
                         return this.retry();
                     } else {
-                        return Promise.reject(err);
+                        return Observable.throw(err);
                     }
-                });
+                }
                 break;
             case 'REQUEST_RESULTS':
                 if (this.format === 'time_series_records' || this.format === 'records') {
@@ -210,7 +200,8 @@ export class SumologicQuerier {
                     if (limit === 0) {
                         return Observable.empty();
                     }
-                    return this.doRequest('GET', '/v1/search/jobs/' + this.job.data.id + '/records?offset=' + this.offset + '&limit=' + limit).then((response) => {
+                    try {
+                        let response = await this.doRequest('GET', '/v1/search/jobs/' + this.job.data.id + '/records?offset=' + this.offset + '&limit=' + limit);
                         this.offset += response.data.records.length;
                         if (this.status.data.state === 'DONE GATHERING RESULTS' || this.offset >= this.maximumOffset) {
                             return Observable.from([response.data]);
@@ -221,19 +212,20 @@ export class SumologicQuerier {
                                     return this.transition('REQUEST_STATUS');
                                 }).mergeMap((value: any) => value)
                             );
-                    }).catch((err) => {
+                    } catch (err) {
                         if (this.retryCount < 6 && err.data && err.data.code && err.data.code === 'searchjob.jobid.invalid') {
                             return this.retry();
                         } else {
-                            return Promise.reject(err);
+                            return Observable.throw(err);
                         }
-                    });
+                    };
                 } else if (this.format === 'messages') {
                     let limit = Math.min(this.maximumOffset, this.status.data.messageCount) - this.offset;
                     if (limit === 0) {
                         return Observable.empty();
                     }
-                    return this.doRequest('GET', '/v1/search/jobs/' + this.job.data.id + '/messages?offset=' + this.offset + '&limit=' + limit).then((response) => {
+                    try {
+                        let response = await this.doRequest('GET', '/v1/search/jobs/' + this.job.data.id + '/messages?offset=' + this.offset + '&limit=' + limit);
                         this.offset += response.data.messages.length;
                         if (this.status.data.state === 'DONE GATHERING RESULTS' || this.offset >= this.maximumOffset) {
                             return Observable.from([response.data]);
@@ -244,25 +236,25 @@ export class SumologicQuerier {
                                     return this.transition('REQUEST_STATUS');
                                 }).mergeMap((value: any) => value)
                             );
-                    }).catch((err) => {
+                    } catch (err) {
                         if (this.retryCount < 6 && err.data && err.data.code && err.data.code === 'searchjob.jobid.invalid') {
                             return this.retry();
                         } else {
-                            return Promise.reject(err);
+                            return Observable.throw(err);
                         }
-                    });
+                    };
                 } else {
-                    return Promise.reject({ message: 'unsupported type' });
+                    return Observable.throw({ message: 'unsupported type' });
                 }
                 break;
         }
+        return Observable.throw({ message: 'unexpected status' });
     }
 
-    doRequest(method, path, params = {}) {
+    async doRequest(method, path, params = {}) {
         if (this.datasource.token === 0) {
-            return this.delay(() => {
-                return this.doRequest(method, path, params);
-            }, Math.ceil(1000 / this.datasource.MAX_AVAILABLE_TOKEN));
+            await this.delay(Math.ceil(1000 / this.datasource.MAX_AVAILABLE_TOKEN));
+            return this.doRequest(method, path, params);
         }
 
         let options: any = {
@@ -289,18 +281,18 @@ export class SumologicQuerier {
             }, Math.ceil(1000 / this.datasource.MAX_AVAILABLE_TOKEN));
         }
 
-        return this.backendSrv.datasourceRequest(options).then((response) => {
+        try {
+            let response = await this.backendSrv.datasourceRequest(options);
             if (response.data.status && response.data.status === 404) {
                 return Promise.reject(response);
             }
             return response;
-        }).catch((err) => {
+        } catch (err) {
             if (err.data && err.data.code && err.data.code === 'rate.limit.exceeded') {
                 this.datasource.token = 0;
-                return this.retryable(3, (retryCount) => {
-                    return this.delay(() => {
-                        return this.backendSrv.datasourceRequest(options);
-                    }, this.calculateRetryWait(1000, retryCount));
+                return await this.retryable(3, async (retryCount) => {
+                    await this.delay(this.calculateRetryWait(1000, retryCount));
+                    return this.backendSrv.datasourceRequest(options);
                 }).catch((err) => {
                     console.error('rate limit exceeded');
                     return err;
@@ -311,18 +303,14 @@ export class SumologicQuerier {
                 console.error(err);
                 return Promise.reject(err);
             }
-        });
+        };
     }
 
-    delay(func, wait) {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                func().then(resolve, reject);
-            }, wait);
-        });
+    delay(msec) {
+        return new Promise(resolve => setTimeout(resolve, msec));
     }
 
-    retryable(retryCount, func) {
+    async retryable(retryCount, func) {
         let promise = Promise.reject({}).catch(() => func(retryCount));
         for (let i = 0; i < retryCount; i++) {
             ((i) => {
