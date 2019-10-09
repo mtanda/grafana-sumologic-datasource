fcccad643f8086246b5c79e8bdd9b5ae15734eb9
Mitsuhiro Tanda <mitsuhiro.tanda@gmail.com>
2019-10-09 18:43:55 +0900

    remove ace editor related code

--- src/query_ctrl.ts
+++ src/query_ctrl.ts
@@ -1,7 +1,5 @@
 import './query_parameter_ctrl';
 import { QueryCtrl } from 'grafana/app/plugins/sdk';
-import './mode-sumologic';
-import './snippets/sumologic';
 
 export class SumologicQueryCtrl extends QueryCtrl {
   static templateUrl = 'partials/query.editor.html';
