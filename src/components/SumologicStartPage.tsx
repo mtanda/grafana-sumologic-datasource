dce0434429914a3b8e9582b4c69b45fcc91a18fe
Mitsuhiro Tanda <mitsuhiro.tanda@gmail.com>
2019-10-09 20:46:39 +0900

    explore

---
+++ src/components/SumologicStartPage.tsx
@@ -0,0 +1,8 @@
+import React, { PureComponent } from 'react';
+import { ExploreStartPageProps } from '@grafana/ui';
+
+export default class SumologicStartPage extends PureComponent<ExploreStartPageProps> {
+  render() {
+    return <div className="grafana-info-box grafana-info-box--max-lg"></div>;
+  }
+}
--- src/module.ts
+++ src/module.ts
@@ -2,10 +2,14 @@ import SumologicDatasource from './datasource';
 import { SumologicQueryCtrl } from './query_ctrl';
 import { SumologicAnnotationsQueryCtrl } from './annotations_query_ctrl';
 import { SumologicConfigCtrl } from './config_ctrl';
+import { SumologicQueryField } from './components/SumologicQueryField';
+import SumologicStartPage from './components/SumologicStartPage';
 import { DataSourcePlugin } from '@grafana/ui';
 import { SumologicQuery, SumologicOptions } from './types';
 
 export const plugin = new DataSourcePlugin<SumologicDatasource, SumologicQuery, SumologicOptions>(SumologicDatasource)
   .setConfigCtrl(SumologicConfigCtrl)
   .setQueryCtrl(SumologicQueryCtrl)
-  .setAnnotationQueryCtrl(SumologicAnnotationsQueryCtrl);
+  .setAnnotationQueryCtrl(SumologicAnnotationsQueryCtrl)
+  .setExploreLogsQueryField(SumologicQueryField)
+  .setExploreStartPage(SumologicStartPage);
