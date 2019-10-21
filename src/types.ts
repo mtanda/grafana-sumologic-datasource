1f8a64183b582c01478b0379d5a5e195836fc1fb
Mitsuhiro Tanda <mitsuhiro.tanda@gmail.com>
2019-10-21 16:16:16 +0900

    log panel support

--- src/types.ts
+++ src/types.ts
@@ -6,7 +6,7 @@ export interface SumologicOptions extends DataSourceJsonData {
 
 export interface SumologicQuery extends DataQuery {
   refId: string;
-  format?: 'records' | 'messages' | 'time_series_records';
+  format?: 'records' | 'messages' | 'time_series_records' | 'logs';
   query?: string;
   aliasFormat?: string;
   hide?: boolean;
